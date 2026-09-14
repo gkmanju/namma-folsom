d28558ebfb1a6487c54d5dd1883170baad593347
Manju Kondappa <gkmanju@gmail.com>
2026-09-13 23:42:40 -0700

    Update Fall Picnic gallery

---
+++ config/events-2026.js
@@ -0,0 +1,13 @@
+const events2026 = {
+    fallPicnic: {
+        photos: "https://Ugrana-001.ezconnect.to/apps/photogallery?albumId=GE:1789366478",
+        rsvp: "https://docs.google.com/forms/d/e/1FAIpQLSeYsxc_OhrVawPpKsTWG7ReZtgfbxQCgub4YyQfU3q_2_w6uw/viewform",
+        location: "https://share.google/DDM6rTCv6JxnZZCHI"
+    },
+
+    deepavali: {
+        photos: "",
+        rsvp: "",
+        location: ""
+    }
+};
