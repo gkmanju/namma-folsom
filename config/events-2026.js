550882c32eb2feae51c5cc8c1ab60152e7669d1e
Manju Kondappa <gkmanju@gmail.com>
2026-09-14 20:43:20 -0700

    Add community photo upload link

--- config/events-2026.js
+++ config/events-2026.js
@@ -1,6 +1,7 @@
 const events2026 = {
     fallPicnic: {
         photos: "https://Ugrana-001.ezconnect.to/apps/photogallery?albumId=GE:1789366478",
+        uploads: "https://Ugrana-001.ezconnect.to/share/C7g3Axx-X",
         rsvp: "https://docs.google.com/forms/d/e/1FAIpQLSeYsxc_OhrVawPpKsTWG7ReZtgfbxQCgub4YyQfU3q_2_w6uw/viewform",
         location: "https://share.google/DDM6rTCv6JxnZZCHI"
     },
